9eb05324dc2935a25ddbac5b3c062a936811d1c2
SalamHyped <shibli.salam30@gmail.com>
2025-05-18 22:18:08 +0300

    Update app.js

--- BE/app.js
+++ BE/app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 8801;
+const cors = require('cors');
+const session = require('express-session');
+app.use(express.json());
+const menuRouter = require('./Routes/menu.js');
+const cartRouter = require('./Routes/cart.js');
+const dbMiddleware = require('./dbSingleton.js').dbMiddleware
 
-app.use("/",(req,res)=>{
-    res.json({message:"first step of the final project"})
-})
+app.use(cors({
+  origin: 'http://localhost:5173', // frontend URL
+  credentials: true               // 🔥 this is essential for cookies!
+}));
+app.use(session({
+    secret: 'salam',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false ,maxAge:600000} // For HTTPS use true
+}));
+
+app.use(dbMiddleware);
+app.use('/menu', menuRouter);
+app.use('/cart', cartRouter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
