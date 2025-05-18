bda3d71900ca68a4a95ef4e91aa65b34d2971247
SalamHyped <shibli.salam30@gmail.com>
2025-05-18 22:36:23 +0300

    Update Category.jsx

--- FE/src/components/category/Category.jsx
+++ FE/src/components/category/Category.jsx
@@ -1,1 +1,27 @@
+import classes from './Category.module.css';
+
+import {Link} from 'react-router-dom'; 
+import RoundedPhoto from "../roundedPhoto/RoundedPhoto";
+export default function Category({items=[]}) {
+
+
+
+    return (
+       <div className={classes.category_container}>
+         {items.map((item) => (
+      <div className={classes.item_wrapper} key={item.category_id}>
+       <Link  to={`/menu/${item.category_name}`}>
+        <RoundedPhoto src={item.category_photo_url} alt={item.category_name} size={150} borderWidth={3} borderColor="#ffffff" />
+        <div className={classes.itemName}>
+         {item.category_name}
+          </div>
+           
+      </Link>
+      </div>
+         ))}
+      </div>
+  
+  )
+}
+      
 
